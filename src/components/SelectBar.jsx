4b0437cd5e1b78825f4de540a5c7c70dd35b62ed
jonathandsilva2 <jonathandsilva2@gmail.com>
2020-09-12 14:15:44 -0400

    cleaned up unnecessary code

--- src/components/SelectBar.jsx
+++ src/components/SelectBar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from 'react';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { articlesContext } from '../utils/api';
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -6,21 +6,22 @@ const articlesContext = React.createContext();
 function ContextProvider({ children }) {
   const NYT_API = async (topic) => {
     const key = 'uKKnzk0uk0UArBEk8qp1PDWNW3kZfPFt';
-    let articles1 = [];
-    let res = await axios.get(
-      `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${key}`,
-    );
+    const url = `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${key}`;
+
+    let articles = [];
+    let res = await axios.get(url);
 
     for (let i = 0; i < res.data.results.length; i++) {
       if (res.data.results[i].multimedia) {
-        articles1.push(res.data.results[i]);
+        articles.push(res.data.results[i]);
       }
-      if (articles1.length === 12) {
+
+      if (articles.length === 12) {
         break;
       }
     }
 
-    const newArticles = articles1.reduce((acc, article) => {
+    const newArticles = articles.reduce((acc, article) => {
       acc.push({
         title: article.title,
         caption: article.abstract,
